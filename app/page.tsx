526fd51140687343dd823f6d563091b432580c98
Sebastián Bolaños Serrano <sebasbose@gmail.com>
2025-11-29 13:50:07 -0600

    Implement API routes for event management with Cloudinary integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import {events} from "@/lib/constants";
+import { IEvent } from '@/database';
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const {events} = await response.json();
+
     return (
         <section>
             <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
@@ -15,7 +20,7 @@ const Page = () => {
                 <h3>Featured Events</h3>
 
                 <ul className="events">
-                    {events.map((event) => (
+                    {events && events.length > 0 && events.map((event: IEvent) => (
                         <li key={event.title}>
                             <EventCard {...event} />
                         </li>
