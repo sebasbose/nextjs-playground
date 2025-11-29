85542c3015bcb9bf68232e17fd4f9d361bd68403
Sebastián Bolaños Serrano <sebasbose@gmail.com>
2025-11-29 14:10:19 -0600

    Add booking functionality to EventDetailsPage with BookEvent component

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import BookEvent from "@/components/BookEvent";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -40,6 +41,8 @@ const EventDetailsPage = async ({params}: {params: Promise<{slug: string}>}) => 
 
     if (!event) return notFound();
 
+    const bookings = 10;
+
     return (
         <section id="event">
             <div className="header">
@@ -80,7 +83,20 @@ const EventDetailsPage = async ({params}: {params: Promise<{slug: string}>}) => 
 
                 {/* Right Side - Booking Form */}
                 <aside className="booking">
-                    <p className="text-lg font-semibold">Book Event</p>
+                    <div className="signup-card">
+                        <h2>Book Your Spot</h2>
+                        {bookings > 0 ? (
+                            <p className="text-sm">
+                                Join {bookings} people who have akready booked their spot!
+                            </p>
+                        ): (
+                            <p className="text-sm">
+                                Be the first to book your spot!
+                            </p>
+                        )}
+                        
+                        <BookEvent/>
+                    </div>
                 </aside>
             </div>
         </section>
